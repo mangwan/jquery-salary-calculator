1477086dce2d107c204cd2e6c330fefdb5d048ec
Mang Wan <vang92@gmail.com>
2019-05-17 15:39:36 -0500

    added handleAddClick function to grab values of input. Append input on the DOM

---
+++ client.js
@@ -0,0 +1,39 @@
+console.log('in js');
+
+let employeeInfo = [
+    {
+    firstName: '',
+    lastName: '',
+    id: '',
+    title: '',
+    annualSalary: ''
+    }
+];
+
+$(document).ready(onReady);
+
+function onReady() {
+//on click collect information from input field
+$('#submitButton').on('click', handleAddClick);    
+}
+
+function handleAddClick() {
+    console.log('in handAddClick');
+    let newfirstName = $('#inputFirstName').val();
+    let newlastName = $('#inputLastName').val();
+    let newId = $('#inputIdNumber').val();
+    let newTitle = $('#inputTitle').val();
+    let newAnnualSalary = $('#InputAnnualSalary').val();
+
+$('#employeeInfoTable').append(
+`<tr>
+    <td>` + newfirstName + `</td>
+    <td>` + newlastName + `</td>
+    <td>` + newId + `</td>
+    <td>` + newTitle + `</td>
+    <td>` + newAnnualSalary + `</td>
+    <td><button class="deleteButton">Delete</button></td>
+</tr>`
+
+);
+}
