a2395a63edc4f949a67b921edf7e43b6d5e03075
Mang Wan <vang92@gmail.com>
2019-05-18 15:39:31 -0500

    removed displayTotalMonthly function. Add conditional. If monthly salary > 0000 then display output in red.

--- client.js
+++ client.js
@@ -2,34 +2,34 @@ console.log('in js');
 
 let employeeInfo = [
     {
-    firstName: '',
-    lastName: '',
-    id: '',
-    title: '',
-    annualSalary: ''
-}
+        firstName: '',
+        lastName: '',
+        id: '',
+        title: '',
+        annualSalary: ''
+    }
 ];
 
 $(document).ready(onReady);
 
 function onReady() {
-//on click collect information from input field
-$('#submitButton').on('click', handleAddClick);    
-$('#employeeInfoTable').on('click', '.deleteButton', handleDeleteClick);
+    //on click collect information from input field
+    $('#submitButton').on('click', handleAddClick);
+    $('#employeeInfoTable').on('click', '.deleteButton', handleDeleteClick);
 }
 
 function handleAddClick() {
     console.log('in handAddClick');
-let newEmployeeInfo = {
-    firstName: $('#inputFirstName').val(),
-    lastName: $('#inputLastName').val(),
-    id: $('#inputIdNumber').val(),
-    title: $('#inputTitle').val(),
-    annualSalary: $('#InputAnnualSalary').val()
-}
-employeeInfo.push(newEmployeeInfo);
-$('#employeeInfoTable').append(
-`<tr>add
+    let newEmployeeInfo = {
+        firstName: $('#inputFirstName').val(),
+        lastName: $('#inputLastName').val(),
+        id: $('#inputIdNumber').val(),
+        title: $('#inputTitle').val(),
+        annualSalary: $('#InputAnnualSalary').val()
+    }
+    employeeInfo.push(newEmployeeInfo);
+    $('#employeeInfoTable').append(
+        `<tr>add
     <td>` + newEmployeeInfo.firstName + `</td>
     <td>` + newEmployeeInfo.lastName + `</td>
     <td>` + newEmployeeInfo.id + `</td>
@@ -37,31 +37,32 @@ $('#employeeInfoTable').append(
     <td>` + newEmployeeInfo.annualSalary + `</td>
     <td><button class="deleteButton">Delete</button></td>
 </tr>`);
-$('#inputFirstName').val('');
-$('#inputLastName').val('');
-$('#inputIdNumber').val('');
-$('#inputTitle').val('');
-$('#InputAnnualSalary').val('');
-calculateTotalMonthly();
-displayTotalMonthly();
+    $('#inputFirstName').val('');
+    $('#inputLastName').val('');
+    $('#inputIdNumber').val('');
+    $('#inputTitle').val('');
+    $('#InputAnnualSalary').val('');
+    calculateTotalMonthly();
 }
 
-function calculateTotalMonthly(){
-let totalYearlySalary = 0;
-for (let i = 0; i < employeeInfo.length; i++){
-    totalYearlySalary += Number( employeeInfo[i].annualSalary);
-}
-let months = 12;
-const MonthlySalary = Number(totalYearlySalary)/Number(months);
-console.log('Total Monthly:', MonthlySalary);
-}
-
-function displayTotalMonthly() {
-    console.log('in displayTotalMonthly');
+function calculateTotalMonthly() {
+    let totalYearlySalary = 0;
+    for (let i = 0; i < employeeInfo.length; i++) {
+        totalYearlySalary += Number(employeeInfo[i].annualSalary);
+    }
+    let months = 12;
+    let monthlySalary = Number(totalYearlySalary) / Number(months);
+    console.log('Total Monthly:', monthlySalary);
+    let el = $('#monthlyDisplayOut');
+    el.empty();
+    el.text(monthlySalary);
+    if(monthlySalary >= $20000){
+        $('#monthlyDisplayOut').css('color', 'red'); 
+    }
 }
 
 
 function handleDeleteClick() {
- console.log('in deleteClick');
- $(this).closest('tr').remove();
+    console.log('in deleteClick');
+    $(this).closest('tr').remove();
 }
